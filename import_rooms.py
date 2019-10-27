a3d595ce5d9a1a9f37cf8d4f7f1aad4a34084975
Omar <omarkhantlp@gmail.com>
2019-10-27 21:27:12 +0300

    adding rooms

--- import_rooms.py
+++ import_rooms.py
@@ -5,12 +5,13 @@ import json
 excel = pd.read_excel("rooms_list.xlsx")
 
 session = requests.Session()
-url = "https://127.0.1.1/rooms/api/admin/locations"
+url_locations = "https://127.0.1.1/rooms/api/admin/locations"
+url_rooms = "https://127.0.1.1/rooms/api/admin/rooms/"
 
 # verify=False because local build so no 
 # SSL cert was needed but requests throws
 # an exception
-response = session.get(url, verify=False)
+response = session.get(url_locations, verify=False)
 
 # using Indico's API requires the X-CSRF-Token
 # and the indico_session cookie to go through.
@@ -33,14 +34,49 @@ for index, row in excel.iterrows():
     # room_name_format is a required field to 
     # make a new location
     data = {
-        "room_name_format":"{building}/{floor}-{number}",
+        "room_name_format":"{building}/{floor}-{room}",
         "name": building_number
     }
     
-    response = session.post(url, headers=headers, cookies=cookies, data=data, verify=False)
+    response = session.post(url_locations, headers=headers, cookies=cookies, data=data, verify=False)
     
 # fetching information of all available locations from
 # the API, mainly location_id, to create a room specific
 # to that location only
-response = session.get(url, headers=headers, cookies=cookies, verify=False)
+response = session.get(url_locations, headers=headers, cookies=cookies, verify=False)
 location_data = json.loads(response.text)
+
+# iterating through each location available
+# in the API to create rooms specific to the 
+# location_id of that location
+for location in location_data:
+
+    location_id = location["id"]
+    building = location["name"]
+    
+    # iterating through each room 
+    for index, row in excel.iterrows():
+        
+        # if the location name (building) from the API
+        # is the same building of the room in the 
+        # excel sheet, then the room is created
+        if(str(row["BUILDING_NUMBER"]) == building):
+            
+            # TODO: search for room owner 
+            
+            # initializing payload with room attributes,
+            # owner is a required field by indico's room creation
+            # API. Manually set to a user for now
+            data = {
+                "building": building,
+                "location_id": location_id,
+                "owner": "User:2",  
+                "floor": row["FLOOR"],
+                "number": row["BUILDING_NUMBER"],
+                "latitude": row["LATITUDE"],
+                "longitude": row["LONGITUDE"],
+                "capacity": row["CAPACITY"]
+            }
+            
+            response = session.post(url_rooms, headers=headers, cookies=cookies, data=data, verify=False)
+
